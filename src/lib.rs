4b142bf68bf42e4b4d2dc1be91aa962f08b701ce
Camille Le Pennec <camille.lpennec@gmail.com>
2024-07-26 20:56:23 +0200

    first commit, currently use wrong winit crate, you should update the correct path in Cargo.toml

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+#[cfg(target_os = "android")]
+mod jni_methods_cache;
+
+#[cfg(target_os = "android")]
+pub use crate::jni_methods_cache::builder::java_methods_build_handle;
+
+#[cfg(target_os = "android")]
+pub use crate::jni_methods_cache::call_java_method;
+#[cfg(target_os = "android")]
+pub use crate::jni_methods_cache::call_java_static_method;
+#[cfg(target_os = "android")]
+pub use jni::signature;
+#[cfg(target_os = "android")]
+pub use jni_methods_cache::{
+    java_method_cache_utils::{JavaArgs, MethodType, ReturnedValue},
+    JavaMethodCache, JavaMethods,
+};
+#[cfg(target_os = "android")]
+pub use signature::Primitive as JniPrimitive;
+#[cfg(target_os = "android")]
+pub use signature::ReturnType;
+
+#[cfg(target_os = "android")]
+pub mod prelude {
+    use super::*;
+
+    pub use jni_methods_cache::{
+        java_method_cache_utils::{JavaArgs, MethodType, ReturnedValue},
+        JavaMethodCache, JavaMethods,
+    };
+    pub use signature::Primitive as JniPrimitive;
+    pub use signature::ReturnType;
+}
